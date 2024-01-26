19bfabd597f309548daf7c8e1a8cba74340f04da
Zin Moe <dev.zinmoe@gmail.com>
2024-01-26 21:05:24 +0630

    Solve warnings

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import App from './App';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import App from "./App";
 
 import store from "./redux/store";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
       <App />
-    </React.StrictMode>
-  </Provider>,
-  document.getElementById('root')
+    </Provider>
+  </React.StrictMode>
 );
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from "redux";
 import chatReducer from "./chatReducer";
 
 const reducers = combineReducers({
-  chatReducer: chatReducer,
+  chat: chatReducer,
 });
 
 export default reducers;
